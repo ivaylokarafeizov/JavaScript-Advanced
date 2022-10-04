fe9befda8d7162ccd85fa911fd5bb7db44150345
Ivaylo Karafeizov <qwerty36758@gmail.com>
2022-10-04 16:32:10 +0300

    Fixed an error

--- Unit Testing and Error Handling - Lab/subSum.js
+++ Unit Testing and Error Handling - Lab/subSum.js
@@ -20,8 +20,8 @@ function subSum(arr, startIndex, endIndex) {
   return sum; 
 } 
 
-console.log(subSum([10, 20, 30, 40, 50, 60], 3, 300)); // 150
-console.log(subSum([1.1, 2.2, 3.3, 4.4, 5.5], -3, 1)); // 3.3
-console.log(subSum([10, 'twenty', 30, 40], 0, 2)); // NaN
-console.log(subSum([], 1, 2)); // 0
-console.log(subSum('text', 0, 2)); // NaN
+subSum([10, 20, 30, 40, 50, 60], 3, 300); // 150
+subSum([1.1, 2.2, 3.3, 4.4, 5.5], -3, 1); // 3.3
+subSum([10, 'twenty', 30, 40], 0, 2); // NaN
+subSum([], 1, 2); // 0
+subSum('text', 0, 2); // NaN
