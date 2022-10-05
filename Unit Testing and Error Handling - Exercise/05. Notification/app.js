9bf3a73295ff207d24496d8101eef82a124024f8
Ivaylo Karafeizov <qwerty36758@gmail.com>
2022-10-05 23:50:34 +0300

    Add 05. Notification

---
+++ Unit Testing and Error Handling - Exercise/05. Notification/app.js
@@ -0,0 +1,9 @@
+function notify(message) {
+  let notification = document.querySelector('#notification');
+  notification.textContent = message;  
+  notification.style.display = 'block';
+
+  notification.addEventListener('click', () => {
+    notification.style.display = 'none';
+  });
+} 
